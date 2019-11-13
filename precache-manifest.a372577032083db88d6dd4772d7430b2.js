fef88194fc5dcb2979d3c029e63a5833c41d7fd0
Keen <keencwilson@gmail.com>
2019-11-13 16:54:02 -0600

    Updates

---
+++ precache-manifest.a372577032083db88d6dd4772d7430b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c835e79dd36667aed0896af9c129c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58d34dda1883da8212dc",
+    "url": "/static/css/2.6cba3815.chunk.css"
+  },
+  {
+    "revision": "21c7892378227545308e",
+    "url": "/static/css/main.dc4ff1b8.chunk.css"
+  },
+  {
+    "revision": "58d34dda1883da8212dc",
+    "url": "/static/js/2.1144f177.chunk.js"
+  },
+  {
+    "revision": "21c7892378227545308e",
+    "url": "/static/js/main.3323f1c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
