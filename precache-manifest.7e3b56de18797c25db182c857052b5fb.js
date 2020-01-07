79a2b7bbaebb58ac14290c8045df6fda2e15b9c2
Keen <kwilson@finsignum.com>
2020-01-06 19:18:40 -0600

    Add mobile dinner noodles page

---
+++ precache-manifest.7e3b56de18797c25db182c857052b5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41feb8da8b834f13335633d16eb17cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "292c0130648d2d2ac481",
+    "url": "/static/css/2.ee71aeed.chunk.css"
+  },
+  {
+    "revision": "215aab2c2b706082b854",
+    "url": "/static/css/main.dc4ff1b8.chunk.css"
+  },
+  {
+    "revision": "292c0130648d2d2ac481",
+    "url": "/static/js/2.9a398812.chunk.js"
+  },
+  {
+    "revision": "215aab2c2b706082b854",
+    "url": "/static/js/main.91826bd5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
