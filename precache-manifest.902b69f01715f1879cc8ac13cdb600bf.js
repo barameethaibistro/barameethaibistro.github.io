fc2daee5be66994f02259e907f698db9325e60af
Keen <kwilson@finsignum.com>
2020-02-07 15:40:42 -0600

    Add article from Feast, Feb 2020 issue

---
+++ precache-manifest.902b69f01715f1879cc8ac13cdb600bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5692424a4b26f59483326bde0c9c553d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d236d76c1d69d6ad79",
+    "url": "/static/css/2.ee71aeed.chunk.css"
+  },
+  {
+    "revision": "f04f031d015bc08476f7",
+    "url": "/static/css/main.adf38c90.chunk.css"
+  },
+  {
+    "revision": "a2d236d76c1d69d6ad79",
+    "url": "/static/js/2.9eccc785.chunk.js"
+  },
+  {
+    "revision": "f04f031d015bc08476f7",
+    "url": "/static/js/main.37b50715.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
