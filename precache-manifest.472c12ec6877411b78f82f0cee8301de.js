b183f0aea13e907deb56403c294c3947af0abc02
Keen <kwilson@finsignum.com>
2020-02-28 17:30:23 -0600

    Update price for Pineapple Fried Rice

---
+++ precache-manifest.472c12ec6877411b78f82f0cee8301de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb96465fce51eafb98762ce70ab75c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d236d76c1d69d6ad79",
+    "url": "/static/css/2.ee71aeed.chunk.css"
+  },
+  {
+    "revision": "5587165106aec0769913",
+    "url": "/static/css/main.adf38c90.chunk.css"
+  },
+  {
+    "revision": "a2d236d76c1d69d6ad79",
+    "url": "/static/js/2.9eccc785.chunk.js"
+  },
+  {
+    "revision": "5587165106aec0769913",
+    "url": "/static/js/main.e922cd13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
