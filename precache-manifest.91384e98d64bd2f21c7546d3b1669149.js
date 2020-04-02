91ff6955bdcef2f8f9fd87f35dbe526d474f5ab0
Keen <kwilson@finsignum.com>
2020-04-02 10:18:52 -0500

    Update pad Key Maw Dinner price and refactor announcement component

---
+++ precache-manifest.91384e98d64bd2f21c7546d3b1669149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea8e0d7d52b6db7d9c1fbff1fb5b46e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71cb04a6961c03d24d71",
+    "url": "/static/css/2.9d34b345.chunk.css"
+  },
+  {
+    "revision": "00dbb126e5c42b60e8af",
+    "url": "/static/css/main.4f99912b.chunk.css"
+  },
+  {
+    "revision": "71cb04a6961c03d24d71",
+    "url": "/static/js/2.f10011f2.chunk.js"
+  },
+  {
+    "revision": "00dbb126e5c42b60e8af",
+    "url": "/static/js/main.a11977c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
