cff4f8218b3218de0d5c1493880868862dc1ea00
Keen <kwilson@finsignum.com>
2020-06-29 13:54:19 -0500

    Remove announcement

---
+++ precache-manifest.9ceac9f330fa623f3e28906ef232e7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f3b3816ef7d6238acb8fde9fa7ee8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19bc7002d8a848bf6f0",
+    "url": "/static/css/2.b20820a9.chunk.css"
+  },
+  {
+    "revision": "7b73aa25eb498423fc60",
+    "url": "/static/css/main.7095c56a.chunk.css"
+  },
+  {
+    "revision": "c19bc7002d8a848bf6f0",
+    "url": "/static/js/2.c7561fe4.chunk.js"
+  },
+  {
+    "revision": "7b73aa25eb498423fc60",
+    "url": "/static/js/main.d90d793e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
