4a7d6a52ce2bc74da29a42562dc3315859a590bc
Keen <keencwilson@gmail.com>
2021-07-19 08:30:41 -0700

    Added holiday alert

---
+++ precache-manifest.8336da9ba06f2bdde441569dfdaf6314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbbec5e6514c069763fdaaebe5ebe41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cab1f38d535def6ed3f",
+    "url": "/static/css/2.f20ed31a.chunk.css"
+  },
+  {
+    "revision": "581fc873eec537cd12c3",
+    "url": "/static/css/main.7d996112.chunk.css"
+  },
+  {
+    "revision": "1cab1f38d535def6ed3f",
+    "url": "/static/js/2.37be4e1d.chunk.js"
+  },
+  {
+    "revision": "581fc873eec537cd12c3",
+    "url": "/static/js/main.42ccce1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
