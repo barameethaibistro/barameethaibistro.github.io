8fe39322fe0faf96f75c82b3d95c45a89ef705d3
Keen <keencwilson@gmail.com>
2022-04-18 12:54:18 -0700

    Update price and add cocktail menu

---
+++ precache-manifest.04b3533173094e7f0b0fc259e838614e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89c10016828b5951afc95db0ad0516a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff883e7fe71a94a494a8",
+    "url": "/static/css/2.f20ed31a.chunk.css"
+  },
+  {
+    "revision": "356a7a5ad6ddf857867c",
+    "url": "/static/css/main.11cca05f.chunk.css"
+  },
+  {
+    "revision": "ff883e7fe71a94a494a8",
+    "url": "/static/js/2.59129e2b.chunk.js"
+  },
+  {
+    "revision": "356a7a5ad6ddf857867c",
+    "url": "/static/js/main.0f1f672d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
